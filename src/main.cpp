06fe238813f1403dd9219c88950cb6d021b01a4d
Elizaveta Cherkashina <134769815+nightly228@users.noreply.github.com>
2025-02-14 15:03:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include "utils.h" // Не забудьте создать этот заголовочный файл
 
+int main() {
+    int a = 5;
+    int b = 3;
+
+    std::cout << "Сложение: " << add(a, b) << std::endl;
+    std::cout << "Вычитание: " << subtract(a, b) << std::endl;
+
+    return 0;
+}
